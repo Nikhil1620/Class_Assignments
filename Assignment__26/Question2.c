009e98ac83b4923e7f38dcf3ba712e299f0ae4d1
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-11-26 13:22:38 +0530

    Add comment block for program description
    Added a comment block to describe the program's functionality.

--- Assignment__26/Question2.c
+++ Assignment__26/Question2.c
@@ -1,3 +1,8 @@
+/*
+    2. Accept number from user and display below pattern.
+    Input : 5
+    Output : 5 # 4 # 3 # 2 # 1 #
+*/
 #include<stdio.h>
 
 void Display(int iNo)
@@ -20,4 +25,5 @@ int main()
     Display(iValue);
 
     return 0;
+
 }
