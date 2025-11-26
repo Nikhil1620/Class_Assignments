6c14dec6913e2654a68a9239d487a1ccc6bd9a81
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-11-26 13:26:31 +0530

    Add comments to Question5.c for clarity
    Added comments to clarify the purpose of the program.

--- Assignment__26/Question5.c
+++ Assignment__26/Question5.c
@@ -1,3 +1,8 @@
+/*
+    5. Accept number from user and display below pattern.
+    Input : 8
+    Output : 2 4 6 8 10 12 14 16 
+*/
 #include<stdio.h>
 
 void Display(int iNo)
@@ -23,4 +28,5 @@ int main()
     Display(iValue);
 
     return 0;
+
 }
