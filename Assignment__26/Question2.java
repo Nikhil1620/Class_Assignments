32eb2c6bc65d9425e92a96243c9a194b4a6e4fd9
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-11-26 13:23:04 +0530

    Add comment block for pattern display functionality
    Added a comment block to explain the pattern display functionality.

--- Assignment__26/Question2.java
+++ Assignment__26/Question2.java
@@ -1,3 +1,9 @@
+/*
+    2. Accept number from user and display below pattern.
+    Input : 5
+    Output : 5 # 4 # 3 # 2 # 1 #
+*/
+
 import java.util.Scanner;
 
 class Pattern
@@ -27,3 +33,4 @@ public class Question2
         pobj.Display(iValue);
     }
 }
+
