41f96495260c8d3951ede68ccb48412ff790b62b
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-11-26 13:27:05 +0530

    Update Question5.java

--- Assignment__26/Question5.java
+++ Assignment__26/Question5.java
@@ -1,3 +1,8 @@
+/*
+    5. Accept number from user and display below pattern.
+    Input : 8
+    Output : 2 4 6 8 10 12 14 16 
+*/
 import java.util.Scanner;
 
 class Pattern
@@ -30,3 +35,4 @@ public class Question5
         pobj.Display(iValue);
     }
 }
+
