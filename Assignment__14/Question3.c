b059edc0b3f7b7ee5cc1cc48f1d952d2e840f004
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-10 20:00:01 +0530

    Update Question3.c

--- Assignment__14/Question3.c
+++ Assignment__14/Question3.c
@@ -1,3 +1,5 @@
+//Write a program which accept number from user and check whether it contains 0 in it or not. 
+
 #include<stdio.h>
 #include<stdbool.h>
 
@@ -39,3 +41,4 @@ int main()
     
     return 0;
 }
+
