474050deea85111072bb8f5d8564269c154308a1
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-27 12:17:40 +0530

    Update Question5.c

--- Assignment__29/Question5.c
+++ Assignment__29/Question5.c
@@ -1,3 +1,12 @@
+/*
+Accept number of rows and number of columns from user and display below pattern.
+Input : iRow = 4 iCol = 4
+Output : 1 2 3 4
+         2 3 4 5
+         3 4 5 6
+         4 5 6 7
+*/
+
 #include<stdio.h>
 
 void Display(int iRow, int iCol)
@@ -33,4 +42,5 @@ int main()
     Display(iValue1, iValue2);
 
     return 0;
+
 }
