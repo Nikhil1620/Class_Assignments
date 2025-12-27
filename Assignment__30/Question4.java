d07bcce9cd3a08227e413f565340bac48a4e1088
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-27 12:23:04 +0530

    Update Question4.java

--- Assignment__30/Question4.java
+++ Assignment__30/Question4.java
@@ -1,3 +1,13 @@
+/*
+Accept number of rows and number of columns from user and display below pattern.
+Input : iRow = 6 iCol = 5
+Output : * * * * *
+         * @ @ @ *
+         * @ @ @ *
+         * @ @ @ *
+         * @ @ @ * 
+*/
+
 import java.util.Scanner;
 
 class Pattern
@@ -53,3 +63,4 @@ public class Question4
         pobj.Display(iValue1, iValue2);
     }
 }
+
